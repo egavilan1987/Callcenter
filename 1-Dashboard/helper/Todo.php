0f43847786031a2f7cf3d7a5622c2c9ae2a82e66
egavilan1987 <enmanuelgc@gmail.com>
2018-07-02 07:32:21 -0400

    7/2/2018 changes
    Research about new fectures to add on the dashboard.

--- 1-Dashboard/helper/Todo.php
+++ 1-Dashboard/helper/Todo.php
@@ -1,21 +1,10 @@
 TODO LIST
 
-Users
-user_role
-
-Change the role to...
-Administrator
-User
-
-https://s3.envato.com/files/101245033/screenshots/26.png
-https://s3.envato.com/files/82407179/Screenshots/Screenshot_5.png
-
-https://www.google.com.do/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiMvbnIgPLbAhWvt1kKHZRLCsUQjRx6BAgBEAU&url=https%3A%2F%2Fcodecanyon.net%2Fitem%2Fuser-management-permission-role-with-chat%2F15765109&psig=AOvVaw05IaUYMy3Rqy9rBpSh8Oea&ust=1530125582536353
-
-https://www.google.com.do/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiMvbnIgPLbAhWvt1kKHZRLCsUQjxx6BAgBEAI&url=https%3A%2F%2Fcodecanyon.net%2Fitem%2Fuser-management-permission-role-with-chat%2F15765109&psig=AOvVaw05IaUYMy3Rqy9rBpSh8Oea&ust=1530125582536353
-
-https://www.google.com.do/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwic5dbygPLbAhWlr1kKHcknBH0Qjxx6BAgBEAI&url=https%3A%2F%2Fcodecanyon.net%2Fitem%2Fadvanced-security-php-registerlogin-system%2F5282621&psig=AOvVaw05IaUYMy3Rqy9rBpSh8Oea&ust=1530125582536353
-
-http://www.phpcodify.com/wp-content/uploads/2017/06/PHP-Registration-Script-Login-CRUD-App-with-Admin-Panel.jpg
+-Study the users on the previous apps
+-Study new dashboards
 
 
+-Finish the user table
+-Start boostrap
+
+https://codecanyon.net/item/user-login-register-and-user-management/9617985?s
