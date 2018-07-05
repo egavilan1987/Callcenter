e659da471932088b01fd8ed224d073eea5ebb689
egavilan1987 <enmanuelgc@gmail.com>
2018-07-05 11:36:37 -0400

    Update updateDepartments.php

--- 1-Dashboard/process/departments/updateDepartments.php
+++ 1-Dashboard/process/departments/updateDepartments.php
@@ -2,14 +2,17 @@
 	session_start();
 	require_once "../../classes/connection.php";
 	require_once "../../classes/Departments.php";
+
 	$currentDate=date("Y-m-d");
 	$iduser="admin";
 	$department=$_POST['department'];
+
 	$data=array(
 		$iduser,
 		$department,
 		$currentDate
 				);
+
 	$obj= new departments();
-	echo $obj->addDepartment($data);
+	echo $obj->updateDepartment($data);
  ?>
