072ddadbce2242fda8cc50aace064652daf4fdd2
egavilan1987 <enmanuelgc@gmail.com>
2018-07-05 16:50:56 -0400

    Update resources.php

--- 1-Dashboard/helper/resources.php
+++ 1-Dashboard/helper/resources.php
@@ -2,3 +2,41 @@ https://codecanyon.net/item/user-login-register-and-user-management/9617985?s
 
 
 
+<!-- Modal -->
+<div class="modal fade" id="updateDepartmentModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+    
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="myModalLabel">Update Department Test</h4>
+        </div>  
+        
+    <div class="modal-body">
+     <div class="container">
+        <h3 class="text-danger">******************************</h3>   
+        <div class="row">
+          <form id="frmUpdateDepartment">
+          <div class="col-sm-8">
+            <div class="form-group col-sm-8" >
+                <input type="text" hidden="" id="idDepartment" name="idDepartment">
+                <label>Department</label>
+                <input type="text" class="form-control input-sm" name="departmentUpdate" id="departmentUpdate">
+            </div>
+          </div>   
+          </form>
+        </div>     
+     </div>
+   </div>
+   
+          <div class="modal-footer">
+            <button id="updateDepartmentBtn" type="button" class="btn btn-warning" data-dismiss="modal">Save changes</button>
+          </div>
+          
+          
+        </div>
+      </div>
+    </div>
+
+
+
