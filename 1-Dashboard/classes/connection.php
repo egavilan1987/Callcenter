7d243050b0f5b113032cd89859a99447441131f6
egavilan1987 <enmanuelgc@gmail.com>
2018-07-05 11:05:30 -0400

    Update connection.php

--- 1-Dashboard/classes/connection.php
+++ 1-Dashboard/classes/connection.php
@@ -4,7 +4,7 @@
 		private $server="localhost";
 		private $user="root";
 		private $password="";
-		private $db="resumes";
+		private $db="administrator";
 		public function connection(){
 			$connection=mysqli_connect($this->server,
 									 $this->user,
