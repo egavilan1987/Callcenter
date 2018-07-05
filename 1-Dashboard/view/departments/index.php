27b53c5043b22184472c851eace90990f5e3b0d3
egavilan1987 <enmanuelgc@gmail.com>
2018-07-05 15:06:46 -0400

    Update index.php

--- 1-Dashboard/view/departments/index.php
+++ 1-Dashboard/view/departments/index.php
@@ -58,10 +58,10 @@
                 <td><a class="btn btn btn-success" href="invoice.php?update=1&id='.$row["id_department"].'"><span class="glyphicon glyphicon-eye-open"></span></a></td>
 
 
-                <td><a class="btn btn btn-warning" data-toggle="modal" data-target="#updateDepartmentModal" onclick="addCandidate('.$row["id_department"].')"><span class="glyphicon glyphicon-edit"></span></a></td>
+                <td><a class="btn btn btn-warning" data-toggle="modal" data-target="#updateDepartmentModal" onclick="addDepartment('.$row["id_department"].')"><span class="glyphicon glyphicon-edit"></span></a></td>
 
 
-                <td><a class="btn btn btn-danger" class="delete"><span class="glyphicon glyphicon-remove" onclick="deleteCandidate('.$row["id_department"].')"></span></a></td>
+                <td><a class="btn btn btn-danger" class="delete"><span class="glyphicon glyphicon-remove" onclick="deleteDepartment('.$row["id_department"].')"></span></a></td>
               </tr>
             ';
           }
@@ -79,158 +79,27 @@
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title" id="myModalLabel">Update Candidates Test</h4>
+            <h4 class="modal-title" id="myModalLabel">Update Department Test</h4>
           </div>
           <div class="modal-body">
 
     <div class="container">
-      <h3 class="text-danger">This part is just for test purpose.</h3>
+      <h3 class="text-danger">******************************</h3>
       <div class="row">
-        <form id="frmUpdateCandidate">
+        <form id="frmUpdateDepartment">
         <div class="col-sm-8">
             <div class="form-group col-sm-8" >
-              <input type="text" hidden="" id="idCandidate" name="idCandidate">
-              <label>Full Name</label>
-              <input type="text" class="form-control input-sm" name="fullNameUpdate" id="fullNameUpdate">
+              <input type="text" hidden="" id="idDepartment" name="idDepartment">
+              <label>Department</label>
+              <input type="text" class="form-control input-sm" name="departmentUpdate" id="departmentUpdate">
             </div>
-              <div class="form-group col-sm-8" >
-              <label>Date of Birth</label>
-                <input type="date" name="dateBirthUpdate" id="dateBirthUpdate" class="form-control">
-              </div>
-
-            <div class="form-group col-sm-8" >
-              <label for="gender">Gender</label>
-              <select name="genderUpdate" id="genderUpdate" class="form-control">
-                <option selected>---</option>
-                <option>Male</option>
-                <option>Female</option>
-                <option>Other</option>
-              </select>
-            </div>
-            <div class="form-group col-sm-8" >
-              <label>Place of Birth</label>
-              <input type="text" class="form-control input-sm" name="placeBirthUpdate" id="placeBirthUpdate">
-              </div>
-            <div class="form-group col-sm-8" >
-              <label>Passport Number/ID</label>
-              <input type="text" class="form-control input-sm" name="idNumberUpdate" id="idNumberUpdate ">
-            </div>
-            <div class="form-group col-sm-8" >
-                    <label>Address</label>
-              <textarea type="text" style="resize:none;" class="form-control input-sm" name="addressUpdate" id="addressUpdate"></textarea>
-            </div>      
-
-
-            <div class="form-group col-sm-8" >
-                    <label>Phone 1</label>
-              <input type="number" class="form-control input-sm" name="phone1Update" id="phone1Update">
-            </div>
-            <div class="form-group col-sm-8" >
-                    <label>Phone 2</label>
-              <input type="number" class="form-control input-sm" name="phone2Update" id="phone2Update">
-            </div>
-            <div class="form-group col-sm-8" >
-              <label>Email</label>
-              <input type="email" class="form-control input-sm" name="emailUpdate" id="emailUpdate" placeholder="name@example.com">
-            </div>
-            <div class="form-group col-sm-8" >
-              <div class="form-group">
-                <label for="exampleFormControlSelect1">Career</label>
-                <select class="form-control" name="careerUpdate" id="careerUpdate">
-                  <option>---</option>
-                  <option>Customer Service Representative</option>
-                  <option>Bilingual Trainer</option>
-                  <option>Sales Representative</option>
-                  <option>Programmer</option>
-                  <option>Other</option>
-                </select>
-              </div>
-            </div>
-            <div class="form-group col-sm-8" >
-              <label>Hours available for work purposes</label>
-              <input type="text" class="form-control input-sm" name="hoursAvailableUpdate" id="hoursAvailableUpdate">
-            </div>
-            <div class="form-group col-sm-8" >
-                    <label>Experience in Call Centers?</label>
-            </div>
-            <div class="form-group col-sm-8" >
-              <div class="form-group">
-                <select class="form-control" name="experienceUpdate" id="experienceUpdate">
-                  <option>---</option>
-                  <option>Yes</option>
-                  <option>No</option>
-                </select>
-              </div>
-            </div>
-            <div class="form-group col-sm-8" >
-                    <label>Resume</label>
-              <input type="file" id="resumeUpdate" name="resumeUpdate">
-            </div>
-          </div>
-
-        <div class="col-sm-6">
-                    <table  class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Languages</th>
-                                <th>None</th>
-                                <th>Low</th>
-                                <th>Medium</th>
-                                <th>High</th>
-                                <th>Native</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    Spanish                                </td>
-                                <td>
-                                    <input type="radio" name="esUpdate" value="none"/>
-                                </td>
-                                <td>
-                                    <input type="radio" name="esUpdate" value="low" />
-                                </td>
-                                <td>
-                                    <input type="radio" name="esUpdate" value="medium" />
-                                </td>
-                                <td>
-                                    <input type="radio" name="esUpdate" value="high" />
-                                </td>
-                                <td>
-                                    <input type="radio" name="esUpdate" value="native" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    English                                </td>
-                                <td>
-                                    <input type="radio" name="enUpdate" value="none" />
-                                </td>
-                                <td>
-                                    <input type="radio" name="enUpdate" value="low" />
-                                </td>
-                                <td>
-                                    <input type="radio" name="enUpdate" value="medium" />
-                                </td>
-                                <td>
-                                    <input type="radio" name="enUpdate" value="high" />
-                                </td>
-                                <td>
-                                    <input type="radio" name="enUpdate" value="native" />
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+        </div>   
         </form>
-
-
       </div>
-    </div>
-          </div>
+     </div>
+   </div>
           <div class="modal-footer">
             <button id="updateDepartmentBtn" type="button" class="btn btn-warning" data-dismiss="modal">Save changes</button>
-
           </div>
         </div>
       </div>
