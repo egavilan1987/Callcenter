b5fcd12eeb185d6973964768fcacc0bc94fd0dc3
egavilan1987 <enmanuelgc@gmail.com>
2018-07-05 07:34:17 -0400

    Todo list

--- 1-Dashboard/helper/Todo.php
+++ 1-Dashboard/helper/Todo.php
@@ -1,28 +1,12 @@
-Tonight To Do: 
+To Do: 
 
+-Start Coding The Dashboard
+--Users
 
--Start Coding The Dashboard(done)
--Query the Inventory's users data(Next)
--Create the Administration database(done)
-
--Install CCleaner
--Unisntall unecesaries software from the my computer
-
--Amazon Claim
--Make the final loan thinking
--English Pronunciation(Done)
- *First session of speak english like americans(done)
- *Study the A sound(done)
- 
 
 In the morning
--Code the Dashboard
 -Study English
 -Study JSON 
 
-
-Tomorrow Todo: 
--Research about adding setting to the dashboard
--Review the dashboard
 
 
