18511d0502a062e9b2c2dfc6aaacc2ddca4977d4
egavilan1987 <enmanuelgc@gmail.com>
2018-07-05 10:51:17 -0400

    Update Todo.php

--- 1-Dashboard/helper/Todo.php
+++ 1-Dashboard/helper/Todo.php
@@ -8,5 +8,10 @@ In the morning
 -Study English
 -Study JSON 
 
+Tonight to do: 
+<p>Logout</p>
+In the profile options try using <p> instead of <samp>
 
+Add The Department Opction
+  Even though it is not part of the first phase of the coding plan just for test and simplicity we are going to use that. 
 
