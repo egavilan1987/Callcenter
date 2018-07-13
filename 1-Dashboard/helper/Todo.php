84af28d1ec3651ab1c888abb19d4f9e861fefb55
egavilan1987 <enmanuelgc@gmail.com>
2018-07-13 17:47:06 -0400

    Update Todo.php

--- 1-Dashboard/helper/Todo.php
+++ 1-Dashboard/helper/Todo.php
@@ -1,15 +1,65 @@
-To Do: 
+On my Job To Do List
 
--Start Coding The Dashboard
---Users
---Study the dashboard code
+1-English
+*Music
+*Pronunciation
+*Writing
+*Reading
+*How to speak like American
 
--Study the the loan
--Magazine review after lunch
+Español
+*Lectura
+*Escritura
+*3 Capitulo de la Biblia
 
 
 
+-
 
-No. de Cuenta 1462 7150 011
+--------------------------------------------
 
-Duglas-->849-607-1443
+HOME TO DO
+
+-Keep coding the Department add button
+ If the field validate does not work continue working with the user module. 
+
+Talk a little bit about the Ensenada Travel(English)
+
+-Pay Banreservas (2,000 pesos)
+-George Payment
+-My pernal finantial
+  *How much I need to take out from my Fonclas
+  *Rethink everything.
+
+
+Finantial
+--------------------
+Anillo
+
+Angel---> 5,700
+Rafa---->  5,500
+----------------------
+Total:     11,200
+Otros       1, 500
+Vecino        700
+-----------------------
+Total        14,400
+
+-----------------------------
+
+
+
+User Management in Asp.net Web API And Angular JS
+https://codecanyon.net/item/user-management-in-aspnet-web-api-and-angular-js/20878694?s_rank=32
+
+Users/Add User
+https://codecanyon.net/item/regex-the-speed-inproved-php-login-website-user-management/20625901?s_rank=46
+
+
+-----------------------------------------------------------
+
+https://codecanyon.net/item/codeigniter-3-user-management-with-privilege-level/19190329?s_rank=10
+
+https://codecanyon.net/item/multilingual-user-management/20386532?s_rank=9
+
+https://codecanyon.net/item/user-management/16508952?s_rank=3
